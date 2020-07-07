ad0ea015ec045f75c8b372d94042410818aa97e0
yletfull <63101316+yletfull@users.noreply.github.com>
2020-07-07 23:29:47 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,7 +30,7 @@ const removeCard = (req, res) => {
             .then(res.status(200).send({ card }))
             .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
         } else {
-          res.status(404).send({ message: 'Нет доступа' });
+          res.status(403).send({ message: 'Нет доступа' });
         }
       } else {
         res.status(404).send({ message: `Карточки с id:'${req.params.cardId}' не существует` });
